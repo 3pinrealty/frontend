9fff9f8621c25c91bfbb90dbb8c56c236144ffc2
3pinrealty <3pinrentals@gmail.com>
2026-05-07 23:23:12 +0530

    env change

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // const defaultApiTarget = 'https://3pin-backend-production.up.railway.app'
-const defaultApiTarget = 'http://localhost:4000'
+// const defaultApiTarget = 'http://localhost:4000'
+
+const defaultApiTarget = 'https://backend-production-7356.up.railway.app/'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
